73506592a0735e23ddb87f721130e28455ab7090
HowardBian <howardbian623@gmail.com>
2021-10-14 19:12:02 -0700

    Added combination.js, still setting up

---
+++ backend/sound-engine/combination.js
@@ -0,0 +1,16 @@
+// comb
+
+
+const combination = {}
+
+combination.build = (combObjArray, soundMap) => {
+
+  for (let combObj of combObjArray) {
+
+    console.log(combObj);
+
+  }
+
+}
+
+export default combination;
