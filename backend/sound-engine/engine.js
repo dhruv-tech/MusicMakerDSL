7bc5171067ad35c9fe81b24c3d9115b340ee4b4b
Dhruv <26849655+dhruv-tech@users.noreply.github.com>
2021-10-15 13:42:13 -0700

    temp changes

--- backend/sound-engine/engine.js
+++ backend/sound-engine/engine.js
@@ -2,23 +2,36 @@
     Sound engine entrypoint
 */
 
+import soundBuilder from "./sound.js";
 const engine = {};
 
 engine.build = (specs) => {
+    return new Promise(async(resolve, reject) => {
 
-    // Validate
+        // Validate
     
-    for (let spec of specs) {
+        for (let [spec, idx] of specs.entries()) {
 
-        let firstKey = Object.keys(spec)[0];
-        if (firstKey == 'error') {
-            console.log('error');
+            let firstKey = Object.keys(spec)[0];
+            if (firstKey == 'error') {
+                reject({error: `Sound/Combination Block ${idx}: ${spec.error}`});
+            }
         }
 
-    }
+        // Process Sounds
 
-    // Process Sounds
-    // Process Combination
+        let sounds = specs.filter((block) => {
+            return block.type == 'Sound';
+        });
+
+        let soundIds = [];
+        for (let sound of sounds) {
+            let id = await soundBuilder.render(sound);
+            //soundIds.append(id);
+        }
+
+        // Process Combination
+    });
 
 }
 
